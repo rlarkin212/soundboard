25e6609b8de996370205fafdde35665c9c3ddd1b
Ryan Larkin <rlarkin212@gmail.com>
2020-05-14 22:59:06 +0100

    massive legend here

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import simpleaudio as sa
+import sys
 
-def play(num):
-    files = ['bruh2.wav', 'nonce.wav', 'massiveLegend.wav']
+
+def playSound(num):
+    files = ['bruh2',
+             'nonce',
+             'massiveLegend',
+             'edp445',
+             'bruh4']
+
     if num < len(files):
-        wave_obj = sa.WaveObject.from_wave_file(f'sounds/{files[num]}')
+        wave_obj = sa.WaveObject.from_wave_file(f'sounds/{files[num]}.wav')
         play_obj = wave_obj.play()
         play_obj.wait_done()
     else:
         print(f'out of index {len(files)} sounds')
 
-play(2)
+
+if __name__ == "__main__":
+    playSound(int(sys.argv[1]))
